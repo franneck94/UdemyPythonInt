21e1771fbe40fb975fa054ad139650f3e934bd72
franneck94 <schaffranek.jan@web.de>
2022-10-14 12:48:34 +0200

    up

--- Chapter6_CPython/c_in_python/main.py
+++ Chapter6_CPython/c_in_python/main.py
@@ -1,4 +1,5 @@
 import math_cpython
+import math_cython
 
 
 def main() -> None:
@@ -7,8 +8,12 @@ def main() -> None:
     min_value = 2.0
     max_value = 4.0
     math_cpython.clip_vector(lst, min_value, max_value)
-
     print(lst)
+
+    lst1 = [float(i) for i in range(3)]  # 0, 1, 2
+    lst2 = [float(i) * 2.0 for i in range(3)]  # 0, 2, 4
+    result = math_cpython.add_vectors(lst1, lst2)
+    print(result)
 
 
 if __name__ == "__main__":
--- Chapter6_CPython/c_in_python/math_cpython/clip.c
+++ Chapter6_CPython/c_in_python/math_cpython/clip.c
@@ -1,6 +1,51 @@
 #define PY_SSIZE_T_CLEAN
 #include "Python.h"
 
+static PyObject *method_add_vectors(PyObject *self, PyObject *args)
+{
+    PyObject *const list_a = NULL;
+    PyObject *const list_b = NULL;
+
+    if (!PyArg_ParseTuple(args, "OO", &list_a, &list_b))
+    {
+        PyErr_SetString(PyExc_TypeError, "error");
+        return NULL;
+    }
+
+    if (!PyList_Check(list_a) || !PyList_Check(list_b))
+    {
+        PyErr_SetString(PyExc_ValueError, "error");
+        return NULL;
+    }
+
+    const Py_ssize_t len_a = PyList_Size(list_a);
+    const Py_ssize_t len_b = PyList_Size(list_b);
+
+    if (len_a != len_b)
+    {
+        PyErr_SetString(PyExc_ValueError, "error");
+        return NULL;
+    }
+
+    PyObject *result = PyList_New(len_a);
+
+    for (Py_ssize_t i = 0U; i < len_a; ++i)
+    {
+        PyObject *const item_a = PyList_GetItem(list_a, i);
+        PyObject *const item_b = PyList_GetItem(list_b, i);
+
+        if (!PyFloat_Check(item_a) || !PyFloat_Check(item_b))
+        {
+            PyErr_SetString(PyExc_ValueError, "Items must be 64bit floats..");
+            return NULL;
+        }
+
+        PyList_SetItem(result, i, PyNumber_Add(item_a, item_b));
+    }
+
+    return result;
+}
+
 static PyObject *method_clip_vector(PyObject *self, PyObject *args)
 {
     PyObject *const list = NULL;
@@ -42,6 +87,7 @@ static PyObject *method_clip_vector(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef math_cpythonMethods[] = {
+    {"add_vectors", method_add_vectors, METH_VARARGS, "CPython Function"},
     {"clip_vector", method_clip_vector, METH_VARARGS, "CPython Function"},
     {NULL, NULL, 0, NULL}
 };
